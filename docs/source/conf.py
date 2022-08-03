21cac35f1a747633da2feafb2ef6f926f64f0224
xiemeiping <1035791926@qq.com>
2022-08-03 10:18:45 +0800

    change the navigation bar

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,10 +50,10 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 
 html_js_files = [
-    'https://ezblock.cc/readDocFile/topHead.js',
+    'https://ezblock.cc/readDocFile/custom.js',
 ]
 html_css_files = [
-    'https://ezblock.cc/readDocFile/topHead.css',
+    'https://ezblock.cc/readDocFile/custom.css',
 ]
 
 # Add any paths that contain custom static files (such as style sheets) here,
